d21d6c4b66952c59aae22be606fce87bc5f5381b
adam <adam@alandstreet.com>
2022-05-04 14:50:27 +1000

    save company profile works

--- src/components/CompanyDetails.vue
+++ src/components/CompanyDetails.vue
@@ -2,40 +2,40 @@
   <div>
     <div class="row q-col-gutter-md">
       <div class="col-3">
-        <q-input v-model="company.name" label="Company Name" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.name" label="Company Name" outlined @keyup.enter="saveCompany"/>
       </div>
       <div class="col-3">
-        <q-input v-model="company.acn_arbn" label="ACN/ARBN" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.acn_arbn" label="ACN/ARBN" outlined @keyup.enter="saveCompany"/>
       </div>
       <div class="col-3">
-        <q-input v-model="company.email" label="Email" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.email" label="Email" outlined @keyup.enter="saveCompany"/>
       </div>
       <div class="col-3">
-        <q-input v-model="company.web" label="Web" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.web" label="Web" outlined @keyup.enter="saveCompany"/>
       </div>
     </div>
     <br/>
     <div class="row q-col-gutter-md">
       <div class="col-6">
-        <q-input v-model="company.address1" label="Address 1" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.address1" label="Address 1" outlined @keyup.enter="saveCompany"/>
       </div>
       <div class="col-6">
-        <q-input v-model="company.address2" label="Address 2" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.address2" label="Address 2" outlined @keyup.enter="saveCompany"/>
       </div>
     </div>
     <br/>
     <div class="row q-col-gutter-md">
       <div class="col-3">
-        <q-input v-model="company.suburb" label="Town" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.suburb" label="Town" outlined @keyup.enter="saveCompany"/>
       </div>
       <div class="col-3">
-        <q-select v-model="company.state" :options="states" label="State" outlined @keyup="$emit('updated', company)"/>
+        <q-select v-model="company.state" :options="states" label="State" outlined @keyup.enter="saveCompany"/>
       </div>
       <div class="col-3">
-        <q-input v-model="company.postcode" label="Postcode" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.postcode" label="Postcode" outlined @keyup.enter="saveCompany"/>
       </div>
       <div class="col-3">
-        <q-input v-model="company.phone" label="Phone" outlined @keyup="$emit('updated', company)"/>
+        <q-input v-model="company.phone" label="Phone" outlined @keyup.enter="saveCompany"/>
       </div>
     </div>
   </div>
@@ -44,6 +44,8 @@
 <script>
 import {defineComponent} from 'vue'
 import {ref} from 'vue'
+import companyApi from "src/api/company"
+import personApi from "src/api/person";
 
 export default defineComponent({
   name: "CompanyDetails",
@@ -67,10 +69,25 @@ export default defineComponent({
       ]
     }
   },
-  emits: {
-    updated(company){
-      // updated company details
-    }
+  emits: ['updated'],
+  methods: {
+    saveCompany() {
+      let self = this
+      companyApi.saveCompanyDetails(self.company).then((result) => {
+        self.$emit('updated', self.company)
+        self.$q.notify({
+          message: 'Success!',
+          color: 'primary'
+        })
+      }).catch((error) => {
+        console.log(error)
+        self.$q.notify({
+          message: error.response.data.message,
+          color: 'accent'
+        })
+      })
+    },
+
   },
   mounted() {},
 })
--- src/components/PersonDetails.vue
+++ src/components/PersonDetails.vue
@@ -2,19 +2,19 @@
   <div>
     <div class="row q-col-gutter-md justify-around">
       <div class="col-4">
-        <q-input v-model="person.first_name" label="First Name" outlined @keyup="$emit('updated', person)"/>
+        <q-input v-model="person.first_name" label="First Name" outlined @keyup.enter="$emit('updated', person)"/>
       </div>
       <div class="col-4">
-        <q-input v-model="person.last_name" label="Last Name" outlined @keyup="$emit('updated', person)"/>
+        <q-input v-model="person.last_name" label="Last Name" outlined @keyup.enter="$emit('updated', person)"/>
       </div>
     </div>
     <br>
     <div class="row q-col-gutter-md justify-around">
       <div class="col-4">
-        <q-input v-model="person.email" label="Email" outlined @keyup="$emit('updated', person)"/>
+        <q-input v-model="person.email" label="Email" outlined @keyup.enter="$emit('updated', person)"/>
       </div>
       <div class="col-4">
-        <q-input v-model="person.phone" label="Phone" outlined @keyup="$emit('updated', person)"/>
+        <q-input v-model="person.phone" label="Phone" outlined @keyup.enter="$emit('updated', person)"/>
       </div>
     </div>
 
@@ -33,11 +33,7 @@ export default defineComponent({
   setup() {
     return {}
   },
-  emits: {
-    updated(person){
-      // updated person details
-    }
-  },
+  emits: ['updated'],
   mounted() {},
 })
 </script>
